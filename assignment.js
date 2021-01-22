e05ba78b6116cf1aa7628f2098366eba626f9b79
awprince69 <kali.nslookup@gmail.com>
2021-01-22 17:50:02 +0600

    comment

--- assignment.js
+++ assignment.js
@@ -1,18 +1,18 @@
+//Kilometer to Meter Calculation
 function kilometerToMeter(kilometer) {
     var meter = kilometer * 1000;
 
-    if(kilometer<0){
-        throw new Error("Enter valid Number");
+    if (kilometer < 0) {
+        return ("Kilometer input is invalid");//validation checker
     }
 
     return meter;
 }
 
-var user1 = kilometerToMeter(-5);
-console.log(user1);
-// var user2 = kilometerToMeter(5);
-// console.log(user2);
+// var user1 = kilometerToMeter(-1);
+// console.log(user1);
 
+//Budget calulator
 
 function budgetCalculator(watch, phone, laptop) {
     var watchPrice = watch * 50;
@@ -21,34 +21,40 @@ function budgetCalculator(watch, phone, laptop) {
 
     var totalBudget = watchPrice + phonePrice + laptopPrice;
 
+    if (watchPrice < 0 || phonePrice < 0 || laptopPrice < 0) {   //validation checker
+        return ("Budgetcalculator input is invalid");
+    }
+
     return totalBudget;
 }
 
-// var result = budgetCalculator(10, 8, 2);
+// var result = budgetCalculator(-1, -1);
 // console.log(result);
 
+//Hotel Cost Counting
 function hotelCost(day) {
     var totalCost = 0;
     if (day <= 10) {
         totalCost = day * 100;
     } else if (day <= 20) {
-        var day10 = 10 * 100;
+        var firstTenDay = 10 * 100;
         var remainingDay = day - 10; //counting remaining  day to get 11-20days
-        var day20 = remainingDay * 80;
-        totalCost = day10 + day20;
+        var secondTenDay = remainingDay * 80;
+        totalCost = firstTenDay + secondTenDay;
     } else {
-        var day10 = 10 * 100;
-        var day20 = 10 * 80;
+        var firstTenDay = 10 * 100;
+        var secondTenDay = 10 * 80;
         var remainingDay = day - 20; //counting remaining  day to get after 20 days
         var restOfTheDay = remainingDay * 50;
-        totalCost = day10 + day20 + restOfTheDay;
+        totalCost = firstTenDay + secondTenDay + restOfTheDay;
     }
     return totalCost;
 }
 
-// var result = hotelCost(91);
-// console.log(result);
+var result = hotelCost(21);
+console.log(result);
 
+//Mega friend name
 function megaFriend(friendsName) {
     var longName = friendsName[0];
     for (var i = 0; i < friendsName.length; i++) {
@@ -57,8 +63,11 @@ function megaFriend(friendsName) {
             longName = element;
         }
     }
+    if (friendsName== ""){
+        return("Array string is invalid");//if input array  string is empty then show error message.
+    }
     return longName;
 }
 
-// var result = megaFriend(["abdul wadud Prince","deepu", "unmoy", "shamim", "abdul wahid Tanzim"]);
+// var result = megaFriend([]);
 // console.log(result);
